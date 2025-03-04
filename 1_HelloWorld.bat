77a96dbcb3ded7e7ed36455138c1da8a158a0447
SamRotthier <132999995+SamRotthier@users.noreply.github.com>
2025-03-04 10:45:00 +0100

    Add files via upload

---
+++ 1_HelloWorld.bat
@@ -0,0 +1,10 @@
+@echo off
+rem @echo off supresses the printing of the command in the prompt
+
+color 0a
+rem color 0a will display green text in the prompt
+
+echo Hello World!
+
+pause
+rem pause will stop the prompt from closing automatically
