1c7a2e88cd08e431d3ed71631ec0ee5a8fbb825c
SamRotthier <132999995+SamRotthier@users.noreply.github.com>
2025-03-04 10:45:30 +0100

    Add files via upload

---
+++ 4_Matrix.bat
@@ -0,0 +1,9 @@
+@echo off
+mode 1000
+rem mode 1000 will deside the screensize
+color 0a
+pause
+
+:a
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto a
